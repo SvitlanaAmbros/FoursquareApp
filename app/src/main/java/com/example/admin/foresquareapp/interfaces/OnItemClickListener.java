45ec6f65ec69dd0932d7da281cac9c20ac1ab3ea
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-12-28 22:59:09 +0200

    create item listener for recycler view

---
+++ app/src/main/java/com/example/admin/foresquareapp/interfaces/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package com.example.admin.foresquareapp.interfaces;
+
+import android.view.View;
+
+import com.example.admin.foresquareapp.dto_place_info.Place;
+
+/**
+ * Created by Admin on 28.12.2017.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(View view, int position);
+}
