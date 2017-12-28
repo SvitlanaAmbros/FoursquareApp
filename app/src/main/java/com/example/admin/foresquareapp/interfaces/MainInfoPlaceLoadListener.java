1e0fd7bd9518ac569c69bbb12df301e07e54b4c5
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-12-28 23:05:07 +0200

    add listeners

---
+++ app/src/main/java/com/example/admin/foresquareapp/interfaces/MainInfoPlaceLoadListener.java
@@ -0,0 +1,9 @@
+package com.example.admin.foresquareapp.interfaces;
+
+/**
+ * Created by Admin on 26.12.2017.
+ */
+
+public interface MainInfoPlaceLoadListener {
+    <T> void infoLoaded(T info);
+}
