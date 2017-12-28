c81ab5db2d02e95780fb079d221f9c8925d0aa50
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-12-28 23:05:53 +0200

    creating presenter for main activity

---
+++ app/src/main/java/com/example/admin/foresquareapp/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.admin.foresquareapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import com.example.admin.foresquareapp.adapters.RVPlaceAdapter;
+import com.example.admin.foresquareapp.dto_place_detail.PlaceInfo;
+import com.example.admin.foresquareapp.dto_place_info.Place;
+import com.example.admin.foresquareapp.interfaces.FindConnectionListener;
+import com.example.admin.foresquareapp.interfaces.OnItemClickListener;
+import com.example.admin.foresquareapp.presenter.MainPresenter;
+import com.example.admin.foresquareapp.view.MainView;
+import com.example.admin.foresquareapp.presenter.MainPresenterImpl;
+import com.example.admin.foresquareapp.receiver.NetworkReceiver;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainView, View.OnClickListener, FindConnectionListener,
+        OnItemClickListener{
+    private MainPresenter mainPresenter;
+
+    private LocationManager locationManager;
+    static final int REQUEST_LOCATION = 1;
+    private NetworkReceiver networkReceiver;
+    private Location location;
+
+    @BindView(R.id.toolbarMain)
+    Toolbar toolbar;
+
+    @BindView(R.id.cancel)
+    ImageView cancel;
+
+    @BindView(R.id.search_text)
+    EditText searchText;
+
+    @BindView(R.id.search)
+    ImageView search;
+
+    @BindView(R.id.filter)
+    ImageView filter;
+
+    @BindView(R.id.rvPlace)
+    RecyclerView rvPlace;
+
+    @BindView(R.id.view_flipper_main_activity)
+    ViewFlipper viewFlipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewFlipper.setDisplayedChild(0);
+
+        initializaComponentToolbar();
+        createNetworkReceiver();
+
+        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        mainPresenter = new MainPresenterImpl(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cancel:
+                searchText.setText("");
+                break;
+            case R.id.filter:
+//                Toast.makeText(this, "New activity", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, FilterActivity.class);
+                startActivityForResult(intent, 1);
+                break;
+            case R.id.search:
+//                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                sendBroadcast();
+
+                location = findCurrentCoordinates();
+
+                break;
+        }
+    }
+
+    public void sendBroadcast() {
+        Intent intent = new Intent();
+        intent.setAction(App.getInstance().BROADCAST_ACTION);
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public void initializaComponentToolbar() {
+        setSupportActionBar(toolbar);
+        cancel.setOnClickListener(this);
+        search.setOnClickListener(this);
+        filter.setOnClickListener(this);
+
+        searchText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (i == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(MainActivity.this, "!!!", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void createListPlace(List<Place> response, List<PlaceInfo> additionalResponse) {
+        viewFlipper.setDisplayedChild(1);
+
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) rvPlace.getLayoutParams();
+        lp.setMargins(0,toolbar.getHeight(),0,0);
+        rvPlace.setLayoutParams(lp);
+
+        rvPlace.setLayoutManager(new LinearLayoutManager(this));
+        RVPlaceAdapter rvPlaceAdapter = new RVPlaceAdapter(response, additionalResponse, this);
+        rvPlace.setAdapter(rvPlaceAdapter);
+
+    }
+
+    @Override
+    public void createNetworkReceiver() {
+        networkReceiver = new NetworkReceiver(this);
+
+        IntentFilter intentFilter = new IntentFilter(App.getInstance().BROADCAST_ACTION);
+        registerReceiver(networkReceiver, intentFilter);
+    }
+
+//    @Override
+//    public void updateRecyclerViewPlace(List<Place> response, List<PlaceInfo> additionalResponse) {
+//        RVPlaceAdapter rvPlaceAdapter = new RVPlaceAdapter(response, additionalResponse);
+//        rvPlace.setAdapter(rvPlaceAdapter);
+//        rvPlaceAdapter.notifyDataSetChanged();
+//    }
+
+
+    public Location findCurrentCoordinates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, REQUEST_LOCATION);
+            return null;
+        }else{
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null){
+               return location;
+
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_LOCATION:
+                findCurrentCoordinates();
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) {return;}
+        int btnSort = data.getIntExtra("sort", R.id.btn_rel);
+
+        mainPresenter.updateRV(btnSort);
+    }
+
+    @Override
+    public void connectionFind() {
+        if(location != null){
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(location.getLatitude()).append(",").append(location.getLongitude());
+
+            mainPresenter.searchInfo(stringBuilder.toString(), searchText.getText().toString());
+        }
+    }
+
+    @Override
+    public void noConnection() {
+        viewFlipper.setDisplayedChild(2);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Intent intent = new Intent(this, PlaceInfoActivity.class);
+        intent.putExtra("id", App.getInstance().getMainModel().getPlaceInfoList().get(position).getId());
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/admin/foresquareapp/presenter/MainPresenterImpl.java
@@ -0,0 +1,157 @@
+package com.example.admin.foresquareapp.presenter;
+
+import com.example.admin.foresquareapp.App;
+import com.example.admin.foresquareapp.R;
+import com.example.admin.foresquareapp.dto_place_detail.PlaceInfo;
+import com.example.admin.foresquareapp.dto_place_detail.ResponceAddedInfoPlace;
+import com.example.admin.foresquareapp.dto_place_info.Place;
+import com.example.admin.foresquareapp.dto_place_info.ResponseByQuery;
+import com.example.admin.foresquareapp.interfaces.AddInfoPlaceLoaderListener;
+import com.example.admin.foresquareapp.interfaces.MainInfoPlaceLoadListener;
+import com.example.admin.foresquareapp.view.MainView;
+import com.example.admin.foresquareapp.interfaces.ServerResponseListener;
+import com.example.admin.foresquareapp.model.PlacesModel;
+import com.example.admin.foresquareapp.server.ServerRequestBuilder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Admin on 22.12.2017.
+ */
+
+public class MainPresenterImpl implements MainPresenter, ServerResponseListener, MainInfoPlaceLoadListener, AddInfoPlaceLoaderListener, BtnSortChangedListener {
+    private MainView mainView;
+    private ServerRequestBuilder serverRequestBuilder;
+    private int position = 0;
+
+    public MainPresenterImpl(MainView mainView) {
+        this.mainView = mainView;
+        serverRequestBuilder = new ServerRequestBuilder(this, this,this);
+    }
+
+    @Override
+    public void searchInfo(String coord, String query) {
+        App.getInstance().setMainModel(new PlacesModel());
+        position = 0;
+        serverRequestBuilder.searchByQuery(coord, query);
+    }
+
+    @Override
+    public void updateRV(int id) {
+        if(id == R.id.btn_rel){
+            mainView.createListPlace(App.getInstance().getMainModel().getPlaceInfoList(),
+                    App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+        }else{
+            List<Place> sortedListPlace = sortListByDistance(App.getInstance().getMainModel().getPlaceInfoList());
+
+            List<PlaceInfo> sortedListAdditionalInfo = sortListAdditionalInfo(sortedListPlace,
+                    App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+
+            mainView.createListPlace(sortedListPlace, sortedListAdditionalInfo);
+        }
+    }
+
+    @Override
+    public <T> void successfulRequest(T response) {
+
+    }
+
+    @Override
+    public void errorRequest() {
+
+    }
+
+    @Override
+    public <T> void infoLoaded(T response) {
+        ResponseByQuery responseByQuery = (ResponseByQuery) response;
+        App.getInstance().getMainModel().setPlaceInfoList(responseByQuery.getResponse().getVenues());
+        for (int i = 0; i < App.getInstance().getMainModel().getPlaceInfoList().size(); i++) {
+            serverRequestBuilder.searchAddInfoPlace(App.getInstance().getMainModel().getPlaceInfoList().get(i).getId());
+        }
+    }
+
+    @Override
+    public <T> void additionalInfoLoaded(T response) {
+        position++;
+        ResponceAddedInfoPlace responceAddedInfoPlace = (ResponceAddedInfoPlace) response;
+        App.getInstance().getMainModel().getAdditionalPlaceInfoList().add(responceAddedInfoPlace.getResponse().getVenue());
+
+        if(position == App.getInstance().getMainModel().getPlaceInfoList().size() - 1) {
+            if(App.getInstance().getMainModel().getDefaultBtnSortBy() == R.id.btn_rel){
+                mainView.createListPlace(App.getInstance().getMainModel().getPlaceInfoList(),
+                        App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+            }else{
+                List<Place> sortedListPlace = sortListByDistance(App.getInstance().getMainModel().getPlaceInfoList());
+
+                List<PlaceInfo> sortedListAdditionalInfo = sortListAdditionalInfo(sortedListPlace,
+                        App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+
+                mainView.createListPlace(sortedListPlace, sortedListAdditionalInfo);
+            }
+        }
+    }
+
+    public List<Place> sortListByDistance(List<Place> placeInfoList) {
+        int[] number = new int[placeInfoList.size()];
+        List<Place> placeInfoListForSorting = new ArrayList<>();
+        placeInfoListForSorting.addAll(placeInfoList);
+
+        for (int i = 0; i < placeInfoList.size(); i++) {
+            number[i] = i;
+        }
+
+        Collections.sort(placeInfoListForSorting, new Comparator<Place>() {
+            @Override
+            public int compare(Place pInfo, Place nInfo) {
+                // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
+                return pInfo.getLocation().getDistance() > nInfo.getLocation().getDistance() ? 1 :
+                        (pInfo.getLocation().getDistance() < nInfo.getLocation().getDistance()) ? -1 : 0;
+            }
+        });
+
+        return placeInfoListForSorting;
+    }
+
+    public List<PlaceInfo> sortListAdditionalInfo(List<Place> placeInfoList,List<PlaceInfo> additionalPlaceInfoList){
+        List<PlaceInfo> additionalPlaceInfoListForSorting = new ArrayList<>();
+
+        for (int i = 0; i < placeInfoList.size(); i++) {
+            additionalPlaceInfoListForSorting.add(findPlaceById(placeInfoList.get(i).getId(), additionalPlaceInfoList));
+        }
+
+        return additionalPlaceInfoList;
+    }
+
+    public PlaceInfo findPlaceById(String id, List<PlaceInfo> additionalPlaceInfoList){
+        PlaceInfo placeInfo = new PlaceInfo();
+
+        for (int i = 0; i < additionalPlaceInfoList.size(); i++) {
+            if(additionalPlaceInfoList.get(i).getId() == id){
+                placeInfo = additionalPlaceInfoList.get(i);
+                break;
+            }
+        }
+
+        return placeInfo;
+    }
+
+    @Override
+    public void changeSortingByChanged() {
+        if(App.getInstance().getMainModel().getDefaultBtnSortBy() == R.id.btn_rel){
+            mainView.createListPlace(App.getInstance().getMainModel().getPlaceInfoList(),
+                    App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+        }else{
+            List<Place> sortedListPlace = sortListByDistance(App.getInstance().getMainModel().getPlaceInfoList());
+
+            List<PlaceInfo> sortedListAdditionalInfo = sortListAdditionalInfo(sortedListPlace,
+                    App.getInstance().getMainModel().getAdditionalPlaceInfoList());
+
+            mainView.createListPlace(sortedListPlace, sortedListAdditionalInfo);
+        }
+    }
+
+
+}
