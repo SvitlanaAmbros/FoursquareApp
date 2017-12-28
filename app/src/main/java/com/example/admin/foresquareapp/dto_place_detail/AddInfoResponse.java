f6943ba3284c3635be9c235464b484588018bcc7
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-12-28 23:04:49 +0200

    add response data type

---
+++ app/src/main/java/com/example/admin/foresquareapp/dto_place_detail/AddInfoResponse.java
@@ -0,0 +1,19 @@
+package com.example.admin.foresquareapp.dto_place_detail;
+
+/**
+ * Created by Admin on 26.12.2017.
+ */
+
+public class AddInfoResponse {
+    private PlaceInfo venue;
+
+    public PlaceInfo getVenue() {
+        return venue;
+    }
+
+    public void setVenue(PlaceInfo venue) {
+        this.venue = venue;
+    }
+
+
+}
