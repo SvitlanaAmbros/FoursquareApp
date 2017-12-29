f3de27fe7abfe4a0822ad9380c9d03fb6bc34e70
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-12-29 02:41:52 +0200

    display detail place

--- app/src/main/java/com/example/admin/foresquareapp/presenter/PlaceInfoPresenterImpl.java
+++ app/src/main/java/com/example/admin/foresquareapp/presenter/PlaceInfoPresenterImpl.java
@@ -33,6 +33,31 @@ public class PlaceInfoPresenterImpl implements PlaceInfoPresenter {
         item.putString("description", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).getDescription());
         item.putDouble("rate", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).getRating());
         item.putString("colorRate", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).getRatingColor());
+        item.putString("address", App.getInstance().getMainModel().getPlaceInfoList().get(ind).getLocation().getAddress());
+        item.putInt("likes", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).getLikes().getCount());
+
+        if(App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).
+                getBestPhoto().getPrefix() != null && App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).
+                getBestPhoto().getSuffix() != null) {
+            item.putString("imgpref", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).
+                    getBestPhoto().getPrefix());
+            item.putString("imgsuf", App.getInstance().getMainModel().getAdditionalPlaceInfoList().get(ind).
+                    getBestPhoto().getSuffix());
+        }
         placeInfoView.setPlaceData(item);
+    }
+
+    @Override
+    public Bundle getLocation(String id) {
+        Bundle bundle = new Bundle();
+
+        for (int i = 0; i < App.getInstance().getMainModel().getPlaceInfoList().size(); i++) {
+            if(App.getInstance().getMainModel().getPlaceInfoList().get(i).getId().equals(id)){
+                bundle.putDouble("lat", App.getInstance().getMainModel().getPlaceInfoList().get(i).getLocation().getLat());
+                bundle.putDouble("lng", App.getInstance().getMainModel().getPlaceInfoList().get(i).getLocation().getLng());
+                break;
+            }
+        }
+        return bundle;
     }
 }
